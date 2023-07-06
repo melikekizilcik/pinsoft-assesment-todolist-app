4f4bdd1193378db32f0fe341721bf8782c2f1444
Melike Kizilcik <melikeekizilcikk@gmail.com>
2023-07-06 11:20:56 +0300

    firebase eklendi

---
+++ src/services/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDPHP1Q0uLSGYSNBtxKpPM6_zEXiR9ZME4",
+  authDomain: "pinsoft-assesment-todolist.firebaseapp.com",
+  projectId: "pinsoft-assesment-todolist",
+  storageBucket: "pinsoft-assesment-todolist.appspot.com",
+  messagingSenderId: "1009710810611",
+  appId: "1:1009710810611:web:4aff6d066d1c6168dc3e86",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
